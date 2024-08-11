31997acae6239d1d715032c54a0c41a2cdbdf85a
kasanek <kastanek.martin2@gmail.com>
2024-08-11 22:29:49 +0200

    Fix GenerateKeys command

--- app/Console/Commands/GenerateKeys.php
+++ app/Console/Commands/GenerateKeys.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Crypto\Rsa\KeyPair;
 
 class GenerateKeys extends Command
@@ -13,6 +14,8 @@ class GenerateKeys extends Command
 
     public function handle()
     {
+        Storage::makeDirectory('keys');
+
         (new KeyPair())->generate(storage_path('app/keys/private.key'), storage_path('app/keys/public.key'));
 
         $this->info('Crypto keys generated successfully');
